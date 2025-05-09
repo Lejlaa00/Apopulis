b70487e56cf9e1d1ceb9ef1523192007fcfddbc1
Kenan Kravic <kravickenan@gmail.com>
2025-05-09 22:32:56 +0200

    Separate files for endpoints have been added, middleware has been moved to a separate folder, and the overall folder structure has been updated.

--- web-app/backend/app.js
+++ web-app/backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config(); //Loads .env file
-const authMiddleware = require('./authMiddleware');
+const authMiddleware = require('./middleware/authMiddleware');
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
